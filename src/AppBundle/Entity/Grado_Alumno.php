8aea46856d3688e0073b445b4134cf60e33991f1
Lobo <t.albertito@gmail.es>
2018-05-29 22:17:01 +0200

    Terminado el listar alumnos y datos personales, terminar todas las ofertas y añadir un apartado que permita ver las no verificadas y verificarlas

---
+++ src/AppBundle/Entity/Grado_Alumno.php
@@ -0,0 +1,65 @@
+<?php
+namespace AppBundle\Entity;
+/**
+ * Created by PhpStorm.
+ * User: 2DAW
+ * Date: 20/02/2018
+ * Time: 16:19
+ */
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
+
+/**
+ * @ORM\Entity()
+ * @ORM\Table(name="Grado")
+ */
+class Grado
+{
+    public function __construct()
+    {
+    }
+    /**
+     * @ORM\Column(type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+    /**
+     * @ORM\Column(type="string", length=100)
+     * @Assert\NotBlank()
+     */
+    private $Nombre;
+
+    /**
+     * @return mixed
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param mixed $id
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getNombre()
+    {
+        return $this->Nombre;
+    }
+
+    /**
+     * @param mixed $Nombre
+     */
+    public function setNombre($Nombre)
+    {
+        $this->Nombre = $Nombre;
+    }
+
+}
---
+++ src/AppBundle/Entity/Grado_Oferta.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: VSPC-GIANTSV5
+ * Date: 29/05/2018
+ * Time: 15:32
+ */
+
+namespace AppBundle\Entity;
+
+
+class Grado_Oferta
+{
+
+}
