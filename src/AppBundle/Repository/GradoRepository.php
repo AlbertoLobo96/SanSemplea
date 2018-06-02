117580c5bd50c5c96d679b58d1da63c75849148f
Lobo <t.albertito@gmail.es>
2018-06-02 21:31:04 +0200

    Terminado el visualizar oferta del inicio e implantado el editar campos con modal

---
+++ src/AppBundle/Repository/GradoRepository.php
@@ -0,0 +1,39 @@
+<?php
+    namespace AppBundle\Repository;
+
+    use Doctrine\ORM\EntityRepository;
+    use Doctrine\ORM\Tools\Pagination\Paginator;
+/**
+ * Created by PhpStorm.
+ * User: Alberto
+ * Date: 29/05/2018
+ * Time: 12:12
+ */
+class OfertaRepository extends EntityRepository
+{
+    public function GetPagina($PagSize, $PagActual){
+        $em = $this->getEntityManager();
+
+        $dql = "SELECT e FROM AppBundle\Entity\Oferta e ORDER BY e.id ASC";
+
+        $query = $em->createQuery($dql)
+            ->setFirstResult($PagSize *($PagActual-1))
+            ->setMaxResults($PagSize);
+
+        $Paginator = new Paginator($query,$fetchJoinCollection = true);
+        return $Paginator;
+    }
+    public function GetOfertas($PagSize, $PagActual,$id){
+
+        $query = $this->createQueryBuilder('o');
+        $result = $query->select('o')
+            ->join('o.Grados','g')->addSelect('g')
+            ->join('g.Alumnos','a')->addSelect('a')
+            ->andWhere('a.id='.$id)
+            ->setFirstResult($PagSize *($PagActual-1))
+            ->setMaxResults($PagSize)
+            ->getQuery();
+        $Paginator = new Paginator($result,$fetchJoinCollection = true);
+        return $Paginator;
+    }
+}
